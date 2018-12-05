848affbb5b46f26f857234c30873b00fc4cbb710
xup655 <xup655@hotmail.com>
2018-12-05 13:48:44 +0800

    decode home

--- ch17/Decode.js
+++ ch17/Decode.js
@@ -1,50 +1,39 @@
  $(function(){
-    //  從php來的宣告，已知有: message、urls、token...等
     var JS_VARS = JSON.parse(shops);
 
-    // 如果message不是空的，就alert出message
     if (JS_VARS.message != '') {
         alert(JS_VARS.message);
     }
 
-    // 監聽decode鈕
     $('.js_decode_btn').click(function(){
-        // 取得input (要 decode 的) val
-        var text = $.trim($('.js_decode_text').val());
-
-        // 把text送去token，type是 decode
-        $.post(JS_VARS.urls.YSecurity, {
-            'text': text,
-            'type': 'decode',
-            _token: JS_VARS.token
-        }, function(result){
-            // 把回傳值顯示在 (encode 後的) input
-            $('.js_encode_text').val(result.YSecurity_text)
-        }, 'json');
+        transformText('decode', 'encode');
     });
 
-    // 監聽encode鈕
     $('.js_encode_btn').click(function(){
-        // 取得input (要 encode 的) val
-        var text = $.trim($('.js_encode_text').val());
-
-        // 把text送去token，type是 encode
-        $.post(JS_VARS.urls.YSecurity, {
-            'text': text,
-            'type': 'encode',
-            _token: JS_VARS.token
-        }, function(result){
-            // 把回傳值顯示在 (decode 後的) input
-            $('.js_decode_text').val(result.YSecurity_text)
-        }, 'json');
+        transformText('encode', 'decode');
     });
 
-    // 監聽 encode copy 鈕
+    /**
+     * 
+     * @param {string} inputVal 
+     * @param {string} output 
+     */
+    function transformText(inputVal, output){
+        var text = $.trim($('.js_' + inputVal + '_text').val());
+
+        $.post(JS_VARS.urls.YSecurity, {
+            'text': text,
+            'type': inputVal,
+            _token: JS_VARS.token
+        }, function(result){
+            $('.js_' + output +'_text').val(result.YSecurity_text)
+        }, 'json');                     
+    }
+
     $('.js_encode_copy').click(function(){
         clickBtn(encode);
     });
 
-    // 監聽 decode copy 鈕
     $('.js_decode_copy').click(function(){
         clickBtn(decode);
     });
@@ -55,18 +44,19 @@
             field.select();
         var isCopysuccess = copySelectionText();
 
-        resultMsg(isCopysuccess);
+        resultMsg(isCopysuccess, '複製');
     }
 
     /**
      * 
      * @param {boolean} isSuccess 
+     * @param {string} action 
      */
-    function resultMsg(isSuccess) {
+    function resultMsg(isSuccess, action) {
         if (isSuccess) {
-            alert('已複製成功');
-        } else {
-            alert('複製失敗，請手動複製');
+            alert('已' + action + '成功');
+        } else { 
+            alert(action + '失敗，請手動' + action);
         }
     }
 
